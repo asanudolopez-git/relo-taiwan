0a9c5f9c77a58bfecadfd08bb59d9765fd07c002
Alex Sanudo <asanudolopez+git@gmail.com>
2025-07-30 11:18:03 -0300

    linting

--- includes/customer-house-list/post-type.php
+++ includes/customer-house-list/post-type.php
@@ -1,5 +1,6 @@
 <?php
-function register_customer_house_list_post_type() {
+function register_customer_house_list_post_type()
+{
     $labels = array(
         'name' => __('Assignee House Lists', 'houses-theme'),
         'singular_name' => __('Assignee House List', 'houses-theme'),
@@ -33,7 +34,8 @@ function register_customer_house_list_post_type() {
 add_action('init', 'register_customer_house_list_post_type');
 
 // Change the title placeholder
-function change_customer_house_list_title_placeholder($title_placeholder) {
+function change_customer_house_list_title_placeholder($title_placeholder)
+{
     $screen = get_current_screen();
     if ($screen && $screen->post_type === 'customer-house-list') {
         $title_placeholder = 'List Name';
@@ -45,12 +47,13 @@ add_filter('enter_title_here', 'change_customer_house_list_title_placeholder');
 /**
  * Register scripts and styles for the customer house list
  */
-function register_customer_house_list_scripts($hook) {
+function register_customer_house_list_scripts($hook)
+{
     if (is_admin()) {
         // Verificar si estamos en la página de edición del customer-house-list
         global $post_type, $pagenow, $post;
         $current_post_type = '';
-        
+
         if ($pagenow == 'post.php' && isset($_GET['post'])) {
             $current_post_type = get_post_type($_GET['post']);
         } elseif ($pagenow == 'post-new.php' && isset($_GET['post_type'])) {
@@ -58,11 +61,11 @@ function register_customer_house_list_scripts($hook) {
         } elseif (isset($post) && is_object($post)) {
             $current_post_type = $post->post_type;
         }
-        
+
         if ($current_post_type == 'customer-house-list') {
             // Registrar y cargar jQuery primero para asegurar que esté disponible
             wp_enqueue_script('jquery');
-            
+
             // Registrar y cargar el script principal con versión aleatoria para evitar caché
             wp_enqueue_script(
                 'property-search-js',
@@ -71,7 +74,7 @@ function register_customer_house_list_scripts($hook) {
                 rand(1000, 9999),
                 false // Cargar en el head para asegurar que esté disponible antes de cargar la página
             );
-            
+
             // Registrar y cargar los estilos
             wp_enqueue_style(
                 'property-search-css',
@@ -79,7 +82,7 @@ function register_customer_house_list_scripts($hook) {
                 array(),
                 rand(1000, 9999)
             );
-            
+
             // Registrar y cargar el script para exportación PDF
             wp_enqueue_script(
                 'pdf-export-js',
@@ -88,14 +91,14 @@ function register_customer_house_list_scripts($hook) {
                 rand(1000, 9999),
                 false
             );
-            
+
             // Localizar el script con datos necesarios para AJAX
             wp_localize_script(
                 'pdf-export-js',
                 'ClientHouseListPDF',
                 array(
                     'ajax_url' => admin_url('admin-ajax.php'),
-                    'nonce'    => wp_create_nonce('generate_customer_house_list_pdf_nonce')
+                    'nonce' => wp_create_nonce('generate_customer_house_list_pdf_nonce')
                 )
             );
         }
